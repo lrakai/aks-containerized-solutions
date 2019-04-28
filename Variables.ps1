af99f751b48daa7d26299e85363a88754d7b6799
Logan Rakai <lmrakai@gmail.com>
2019-04-28 14:35:39 -0600

    Initial lab scripts

---
+++ Variables.ps1
@@ -0,0 +1,7 @@
+$Lab="aks"
+$Region="WestUS"
+$User="student-lab-01@labscloudacademy.onmicrosoft.com"
+$Pass="labAccount1"
+$PolicyDefinitionName="lab-policy"
+$PolicyAssignmentName="lab-policy-assignment"
+$RoleDefinitionName="Lab Role"
